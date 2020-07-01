74f5c3fd3a0659e04b5a94c83c3fa032251b2770
Fabio <54443264+totalmentecasuale@users.noreply.github.com>
2020-07-01 17:04:26 +0200

    Update sketch.ino
    Now it sends the temp value to Thingspeak (I'm not sending both because Thingspeak would reject two close requests since I have a free account)

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,13 +1,41 @@
 #include <DHT.h>
 
+#include <ESP8266WiFi.h>;
+ 
+#include <WiFiClient.h>;
+ 
+#include <ThingSpeak.h>;
+ 
+const char* ssid = "wifimcu"; //Your Network SSID
+ 
+const char* password = "zoccazocca"; //Your Network Password
+
 // Setup DHT11 pins (D4)
 #define DHT11_PIN 2
 DHT DHT(DHT11_PIN,DHT11);
+//-----------------------
+
+// Thingspeak channel data
+// ID 1092202
+// Write key WRVX1WEF8WHP5M1W
+ 
+WiFiClient client;
+ 
+unsigned long myChannelNumber = 1092202; //Your Channel Number (Without Brackets)
+ 
+const char * myWriteAPIKey = "WRVX1WEF8WHP5M1W"; //Your Write API Key
 
 void setup()
 {
 Serial.begin(115200);
+
+// Starting the sensor
 DHT.begin();
+
+// Connecting to WiFi
+WiFi.begin(ssid, password);
+// Connecting to Thingspeak
+ThingSpeak.begin(client);
 }
 void loop()
 {
@@ -20,5 +48,6 @@ Serial.print("Temperature (C) = ");
 Serial.println(tempC);
 Serial.print("Humidity = ");
 Serial.println(humidity);
-delay(3000);
+ThingSpeak.writeField(myChannelNumber, 1,tempC, myWriteAPIKey); //Update temperature in ThingSpeak
+delay(30000);
 }
